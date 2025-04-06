cc1891e3f7a6e701fdd7d44b9639a29bd3fea3cf
Gaurav Sharma <84325665+Gauravsh25@users.noreply.github.com>
2025-04-06 20:08:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,17 @@
-resource "aws_instance" "My-ec2-2" {
-  ami           = "ami-0dee22c13ea7a9a67"
-  instance_type = "t2.small"
-  key_name      = "kube-demo"
-  user_data     = file("kind.sh")
-  count         = 3
-  #depends_on      = [aws_instance.My-Ubuntu-Server]
-  #security_groups = ["tf-security-group"]
+resource "aws_instance" "My-ec2-1" {
+  ami             = "ami-0dee22c13ea7a9a67"
+  instance_type   = "t2.medium"
+  key_name        = "kube-demo"
+  user_data       = file("kind.sh")
+  security_groups = ["tf-security-group"]
+  root_block_device {
+    volume_size           = 30 # Size in GB
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
   tags = {
-    Name    = "Web-Server-${count.index}"
-    Team    = "Real Device"
-    creator = "gauravsharma25861@gmail.com"
-
+    Name = "Web-Server"
+    Team = "Real Device"
   }
 }
 
